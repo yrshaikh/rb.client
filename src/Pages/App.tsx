3126980f64a0e426fb18e145e8951dd31adac4e5
Yasser shaikh <yrshaikh.mail@gmail.com>
2019-07-25 13:04:56 +0700

    Update App.tsx

--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React from 'react';
+var a = "asas";
 import "./App.scss";
 import "./AntOverrides.scss";
 import {Switch, Route, Redirect} from "react-router-dom";
