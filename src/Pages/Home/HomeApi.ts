7ed1acaf07f9d34d307aaf50e116d2467df46589
Yasser Shaikh <yrshaikh.mail@gmail.com>
2019-08-03 12:07:55 +0700

    Added proxy.

--- src/Pages/Home/HomeApi.ts
+++ src/Pages/Home/HomeApi.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export default class HomeApi {
   public GetPublicMessage(): any {
     return axios
-      .get(`https://localhost:5001/api/home`)
+      .get(`/api/home`)
       .then((response: any) => response.data);
   }
 }
